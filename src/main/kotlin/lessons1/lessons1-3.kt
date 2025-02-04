e70ee943d046ff1ba7ad11477b721279a4995ae1
ЖУРАВЛЕВЫ <dreha@bk.ru>
2025-02-04 18:37:16 +0300

    LK1-3 for verification

---
+++ src/main/kotlin/lessons1/lessons1-3.kt
@@ -0,0 +1,17 @@
+package org.example.lessons1
+
+fun main() {
+
+    val year = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+    print("$hour:$minute")
+
+}
