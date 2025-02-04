714640e6224fd22dfe7b2386f6e42e75c0428d4d
ЖУРАВЛЕВЫ <dreha@bk.ru>
2025-02-04 18:07:20 +0300

    LK1-2 for verification

---
+++ src/main/kotlin/lessons1/lessons1-2.kt
@@ -0,0 +1,17 @@
+package org.example.lessons1
+
+fun main() {
+
+    val numberOfBuyer: Int = 75
+    val adText: String = "Thank you for your purchase"
+
+    println(numberOfBuyer)
+    println(adText)
+
+    var onlineBuyer: Int = 2000
+//    println(onlineBuyer)
+
+    onlineBuyer--
+    println(onlineBuyer)
+
+}
