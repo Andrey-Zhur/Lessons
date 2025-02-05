28b318d7c291d9ed110f6c994d728c02ca6c2c3d
ЖУРАВЛЕВЫ <dreha@bk.ru>
2025-02-05 23:38:21 +0300

    LK2-2 for verification

---
+++ src/main/kotlin/lessons2/LK2-2.kt
@@ -0,0 +1,21 @@
+package org.example.lessons2
+
+fun main() {
+
+    val oldWorker = 50
+    val salaryOldWorker = 30000
+
+    val intern = 30
+    val salaryIntern = 20000
+
+    val payOldWorker = oldWorker*salaryOldWorker
+    val payIntern = intern*salaryIntern
+
+    val totalConsumption = payOldWorker+payIntern
+    val average = totalConsumption/(oldWorker+intern)
+
+    println("Расходы на выплату зарплаты постоянных сотрудников $payOldWorker")
+    println("Общие расходы по ЗП после прихода стажеров $totalConsumption")
+    println("Среднюю ЗП одного сотрудника после устройства стажеров $average")
+
+}
