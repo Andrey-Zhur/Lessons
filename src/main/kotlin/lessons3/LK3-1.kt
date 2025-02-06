a74825d07234c1a6086756c5dcda8b921494326d
ЖУРАВЛЕВЫ <dreha@bk.ru>
2025-02-06 20:19:01 +0300

    LK3-1 for verification

---
+++ src/main/kotlin/lessons3/LK3-1.kt
@@ -0,0 +1,13 @@
+package org.example.lessons3
+
+fun main() {
+
+    val name = "Андрей"
+    val greetingsDay = "Добрый день  "
+    val greetingsEvening = "Добрый вечер "
+
+    val hi = ("$greetingsDay$name, $greetingsEvening$name")
+
+    print(hi)
+
+}
