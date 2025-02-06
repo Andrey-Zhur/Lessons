5a02b10766ea76e3ec14fdf8d82129f5a1d6c378
ЖУРАВЛЕВЫ <dreha@bk.ru>
2025-02-06 19:16:53 +0300

    LK2-4 for verification

--- src/main/kotlin/lessons2/LK2-4.kt
+++ src/main/kotlin/lessons2/LK2-4.kt
@@ -3,13 +3,13 @@ package org.example.lessons2
 fun main() {
 
     val cristRes = 7
-    val ironRess = 11
+    val ironRes = 11
     val bonus = 0.2
 
     val cristResBonus1 = cristRes * bonus
     val cristResBonus2 = cristResBonus1.toInt()
 
-    val ironResBonus1 = ironRess * bonus
+    val ironResBonus1 = ironRes * bonus
     val ironResBonus2 = ironResBonus1.toInt()
 
     println(cristResBonus2)
