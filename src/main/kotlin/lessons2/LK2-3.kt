9429c0c71456a5829b5184a1aacfd1a4cdec07f0
ЖУРАВЛЕВЫ <dreha@bk.ru>
2025-02-06 18:47:12 +0300

    LK2-3 for verification

---
+++ src/main/kotlin/lessons2/LK2-3.kt
@@ -0,0 +1,17 @@
+package org.example.lessons2
+
+const val HOUR = 60
+
+fun main() {
+    val hourDeparture = 9
+    val minuteDeparture = 39
+    val travelTime = 457
+
+    val timeOfDay = (hourDeparture * HOUR + minuteDeparture) + travelTime
+
+    val hourTravel = timeOfDay / 60 % 24
+    val minuteTravel = timeOfDay % HOUR
+
+println("Прибытие поезда $hourTravel:$minuteTravel")
+
+}
