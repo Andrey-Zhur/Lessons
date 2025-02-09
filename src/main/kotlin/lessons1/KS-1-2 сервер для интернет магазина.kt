e0120cbe5a2f8f60a2288c6fd6920042352ccfb4
ЖУРАВЛЕВЫ <dreha@bk.ru>
2025-02-09 11:59:32 +0300

    KS-1-2 for verification

--- src/main/kotlin/lessons1/KS-1-2 сервер для интернет магазина.kt
+++ src/main/kotlin/lessons1/KS-1-2 сервер для интернет магазина.kt
@@ -3,7 +3,7 @@ package org.example.lessons1
 fun main() {
 
     val numberOfBuyer: Int = 75
-    val adText: String = "Thank you for your purchase"
+    val adText: String = "Thank you for your purchase."
 
     println(numberOfBuyer)
     println(adText)
