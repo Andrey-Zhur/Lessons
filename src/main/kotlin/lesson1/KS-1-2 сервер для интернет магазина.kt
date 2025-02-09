ef906119b2ca857bce44d5378453b95723d54cdf
ЖУРАВЛЕВЫ <dreha@bk.ru>
2025-02-09 12:03:24 +0300

    KS-1-2 for verification

---
+++ src/main/kotlin/lesson1/KS-1-2 сервер для интернет магазина.kt
@@ -0,0 +1,17 @@
+package org.example.lesson1
+
+fun main() {
+
+    val numberOfBuyer: Int = 75
+    val adText: String = "Thank you for your purchase"
+
+    println(numberOfBuyer)
+    println(adText)
+
+    var onlineBuyer1: Int = 2000
+//    println(onlineBuyer)
+
+    val onlineBuyer2 = onlineBuyer1 - 1
+    println(onlineBuyer2)
+
+}
