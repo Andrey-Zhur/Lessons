21b0ba4c9ed0f23b9d0a7238e26dfa6b236543ff
ЖУРАВЛЕВЫ <dreha@bk.ru>
2025-02-12 10:24:23 +0300

    KS-2-4-added constants

--- src/main/kotlin/lessons2/KS-2-4-arithmetic.kt
+++ src/main/kotlin/lessons2/KS-2-4-arithmetic.kt
@@ -1,15 +1,19 @@
 package org.example.lessons2
 
+const val ONEHUNDRED = 100.0
+
 fun main() {
 
     val cristRes = 7
     val ironRes = 11
-    val bonus = 0.2
+    val bonus = 20
 
-    val cristResBonus1 = cristRes * bonus
+    val bonusConversion = bonus / ONEHUNDRED
+
+    val cristResBonus1 = cristRes * bonusConversion
     val cristResBonus2 = cristResBonus1.toInt()
 
-    val ironResBonus1 = ironRes * bonus
+    val ironResBonus1 = ironRes * bonusConversion
     val ironResBonus2 = ironResBonus1.toInt()
 
     println(cristResBonus2)
