a73a58b465a549705c8e2912c59125f8c4f9c24f
ЖУРАВЛЕВЫ <dreha@bk.ru>
2025-02-12 10:29:43 +0300

    KS-2-5-added constants

--- src/main/kotlin/lessons2/KS-2-5-arithmetic.kt
+++ src/main/kotlin/lessons2/KS-2-5-arithmetic.kt
@@ -1,12 +1,15 @@
 package org.example.lessons2
 
+const val ONEHUNGRED = 100
+
 fun main() {
 
     val amountOfMoney = 70000.0
-    val percent = 16.7 / 100
+    val percent = 16.7
     val years = 20
+    val interestRate = percent / ONEHUNGRED
 
-    val finalAmount = amountOfMoney * Math.pow(1 + percent, years.toDouble())
+    val finalAmount = amountOfMoney * Math.pow(1 + interestRate, years.toDouble())
 
     print("%.3f".format(finalAmount))
 
